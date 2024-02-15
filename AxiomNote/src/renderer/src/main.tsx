9359fdbbb7af0b36a3770b0ea3c8fb7c373e18ca
yuri <iuriayolphineves@gmail.com>
2024-02-15 22:52:33 +0000

    style: add system-based theme
    Not much to say here, just a way for the system to decide between light and dark mode.

--- AxiomNote/src/renderer/src/main.tsx
+++ AxiomNote/src/renderer/src/main.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from './components/theme-provider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <App />
     </ThemeProvider>
   </React.StrictMode>
