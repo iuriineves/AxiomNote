ed756076f159bf4c1a47343cd2de20fcf23a5a3b
yuri <iuriayolphineves@gmail.com>
2024-02-15 22:53:30 +0000

    feat: change the default topbar
    This will be useful in the future as I intend to add a search bar/command palette on top of the app.

--- AxiomNote/src/renderer/src/components/top-bar.tsx
+++ AxiomNote/src/renderer/src/components/top-bar.tsx
@@ -1,19 +1,31 @@
 const { ipcRenderer } = require('electron')
 
-const iconStyle = " opacity-50 material-symbols-rounded cursor-pointer select-none icon-variation-settings no-drag text-2xl px-3 py-1 transition-colors hover:bg-zinc-900 hover:opacity-100"
+const iconStyle =
+  ' opacity-50 material-symbols-rounded text-muted-foreground cursor-pointer select-none icon-variation-settings no-drag px-3 transition-colors hover:opacity-100 hover:text-foreground'
 
 export default function TopBar(): JSX.Element {
   return (
     <div className="w-screen flex top-0 fixed drag content-end justify-end items-center">
-      <span className=" opacity-50 select-none font-thin ml-3 pr-1">AxiomNote</span>
+      <span className="font-Noto text-muted-foreground select-none font-extralight ml-3 pr-1">
+        AxiomNote
+      </span>
       <div className="drag w-fill" />
-      <span className={iconStyle} onClick={() => ipcRenderer.send('minimize')}>
+      <span
+        className={iconStyle + ' hover:bg-muted text-xl py-0.5'}
+        onClick={() => ipcRenderer.send('minimize')}
+      >
         remove
       </span>
-      <span className={iconStyle + ' text-xl px-3.5 py-1.5'} onClick={() => ipcRenderer.send('maximize')}>
+      <span
+        className={iconStyle + ' hover:bg-muted text-base px-3.5 py-1'}
+        onClick={() => ipcRenderer.send('maximize')}
+      >
         check_box_outline_blank
       </span>
-      <span className={iconStyle + ' hover:bg-red-700'} onClick={() => ipcRenderer.send('close')}>
+      <span
+        className={iconStyle + ' hover:bg-destructive text-xl py-0.5'}
+        onClick={() => ipcRenderer.send('close')}
+      >
         close
       </span>
     </div>
